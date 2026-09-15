bb75f1f09155ac8a0ef06b79c9e3a27b375bf529
toyamahikari12-hub <toyamahikari12@gmail.com>
2026-09-16 02:54:25 +0800

    Update Channel.kt

--- app/src/main/java/com/animatv/player/model/Channel.kt
+++ app/src/main/java/com/animatv/player/model/Channel.kt
@@ -3,10 +3,20 @@ package com.animatv.player.model
 import com.google.gson.annotations.SerializedName
 
 class Channel {
+
     var name: String? = null
-    @SerializedName("stream_url")
+
+    @SerializedName(value = "stream_url", alternate = ["url"])
     var streamUrl: String? = null
+
     @SerializedName("drm_name")
     var drmName: String? = null
+
     var logo: String? = null
+
+    @SerializedName("ua")
+    var userAgent: String? = null
+
+    @SerializedName("referrer")
+    var referrer: String? = null
 }
